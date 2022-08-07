ac68e4b9b4108fe6b7525b78d165d1fb5b968daf
Aleksandar Pavlovic <sweetsare42@gmail.com>
2022-08-07 18:08:02 +0200

    Now you cannot divide with a 0

--- javascript.js
+++ javascript.js
@@ -12,52 +12,28 @@ let operators = document.querySelector(".operators");
 
 let currentInputDisplay = document.querySelector("#currentInput");
 let previousInputDisplay = document.querySelector("#previousInput");
-
+let currentOperatorDisplay = document.querySelector("#currentOperator");
 
 
 let displayCurrent = () => currentInputDisplay.textContent = currentInput;
 let displayPrevious = () => previousInputDisplay.textContent = previousInput;
+let displayCurrentOperator = () => currentOperatorDisplay.textContent = eligibleOperators[operator];
 
-
-
-/* for numbers on keypress or click we write that number
-into current input
-
-on operator +-* or / we sum up current input with previous input
-
-and write that operator in front of previous input number
-will have to consider how to change it if they change their mind
-
-on = we just sum current and previous with last operator
-
-*/ 
-
-
-/*object.addEventListener("keydown", myScript); */
-
-/* eligible keys that can be pressed */
-
-/*when we press one of the above buttons
-add them into currentInput array */
-
-/* Operations */
 
 
 keypad.addEventListener('click', (event) => {
     if (event.target.matches("#buttonsClear")){
         currentInput = "";
         previousInput = "";
-        operator = "+";
+        operator = 0;
         displayCurrent();
         displayPrevious();
     } 
 
   })
 
-
-
-
 window.addEventListener("keypress", event => {
+    displayCurrentOperator();
     if (currentInput.includes(".")===true && (currentInput.length)-3 === currentInput.indexOf(".")){
     return;
     } else{
@@ -68,8 +44,6 @@ window.addEventListener("keypress", event => {
         displayCurrent();
         }}}
     });
-
-
 window.addEventListener("keydown", event => {
         console.log(event.key)
     if (event.key === "Backspace"){
@@ -82,34 +56,21 @@ if (event.key === "."){
         displayCurrent();
 }
 }
-
 if (eligibleOperators.includes(event.key)===true){
-    operator = eligibleOperators.indexOf(event.key);
-    switch(operator) {
-        case 0:
-            addition();
-            calculate();
-        break;
-        case 1:
-            subtraction();
-            calculate();
-        break;
-        case 2:
-            multiplication();
-            calculate();
-        break;
-        case 3:
-            divison();
-            calculate();
-        break;
-          
-      }
+    if (event.key==="/" && previousInput == 0 || currentInput == 0){
+        previousInput = "Cannot divide with a 0";
+        displayPrevious();
+        previousInput="";
+        currentInput="";
+    } else{
+        operator = eligibleOperators.indexOf(event.key);
+        calculate();
+    }
 }
 
-if (event.key === "Enter"){
-    
-    previousInput = Number(previousInput) + Number(currentInput); /*here we calculate */
+if (event.key === "Enter" || event.key === "=") {  
     calculate();
+
 }
 
 
@@ -122,17 +83,40 @@ if (event.key === "Enter"){
 let addition = () => previousInput = Number(currentInput) + Number(previousInput);
 let subtraction = () => previousInput = Number(currentInput) - Number(previousInput);
 let multiplication = () => previousInput =Number(currentInput) * Number(previousInput);
-let divison = () => previousInput = Number(currentInput) / Number(previousInput);
+let divison = () => previousInput = Number(previousInput) / Number(currentInput);
 
 
 
-function calculate() {
+
+function previousInputShown() {
 previousInput = previousInput.toFixed(2).replace(/[.,]00$/, ""); 
 /*Shows two decimal places only if there are decimals */
 
 previousInput = String(previousInput);
-
 currentInput = "";
-
 displayCurrent();
 displayPrevious();}
+
+
+
+
+function calculate(){
+    switch(operator) {
+    case 0:
+        addition();
+        previousInputShown();
+    break;
+    case 1:
+        subtraction();
+        previousInputShown();
+    break;
+    case 2:
+        multiplication();
+        previousInputShown();
+    break;
+    case 3:
+        divison();
+        previousInputShown();
+    break;
+  }
+}
